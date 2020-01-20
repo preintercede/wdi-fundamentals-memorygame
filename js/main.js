a295fdfc88478b8b3805d01ac485941bb92c4282
Dylan Woo <dylanw@uci.edu>
2020-01-19 17:27:38 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,18 +2,28 @@ console.log("Up and running");
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsinPlay = [];
-let cardOne = cards[0];
-cardsinPlay.push(cardOne);
-let cardTwo = cards[2];
-cardsinPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsinPlay.length === 2) {
+function checkForMatch() {
   if (cardsinPlay[0] === cardsinPlay[1]) {
-    alert("You found a match");
+    console.log("You found a match!");
   } else {
-    alert("Sorry try again");
+    console.log("Sorry, try again.");
   }
 }
+
+function flipCard(cardId) {
+  cardsinPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+}
+
+flipCard(2);
+flipCard(3);
+checkForMatch();
+
+// if (cardsinPlay.length === 2) {
+//   if (cardsinPlay[0] === cardsinPlay[1]) {
+//     alert("You found a match");
+//   } else {
+//     alert("Sorry try again");
+//   }
+// }
